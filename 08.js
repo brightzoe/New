862ea3cdc3ba53ccd4f8dc9233a3b8001edd9688
Zoe Liu <2577213154@qq.com>
2020-06-03 22:30:18 +0800

    06/03/2020 Wed 22:30:18.18

--- 08.js
+++ 08.js
@@ -1,129 +1,348 @@
-const ap = new APlayer({
-  container: document.getElementById('aplayer'),
-  mini: false,
-  autoplay: false,
-  theme: '#e9e9e9',
-  loop: 'all',
-  order: 'random',
-  preload: 'auto',
-  volume: 0.7,
-  mutex: true,
-  listFolded: false,
-  listMaxHeight: 90,
-  lrcType: 1,
-  audio: [
-    {
-      name: 'name',
-      artist: 'artist',
-      url:
-        'https://zhangchen2397.github.io/audioPlayer/example/asset/music/once.mp3',
-      cover: 'https://cdn.jsdelivr.net/gh/brightzoe/img/ieslpod',
-      lrc: '[00:00.00]APlayer\n[00:04.01]is\n[00:08.02]amazing',
-
-      theme: '#ebd0c2',
-    },
-  ],
-})
-ap.lrc.hide() //不显示歌词
-createBtn()
-document.getElementById('toggle').onclick = toggle
-document.getElementsByClassName('aplayer-pic').onclick = toggle
-document.getElementById('stop').onclick = stop
-document.getElementById('rew').onclick = rew
-document.getElementById('ff').onclick = ff
-
-function createBtn() {
-  //创建四个button
-  let aplayer = document.getElementById('aplayer')
-  let btn1 = document.createElement('button')
-  let btn2 = document.createElement('button')
-  let btn3 = document.createElement('button')
-  let btn4 = document.createElement('button')
-  btn1.id = 'toggle'
-  btn1.title = 'play/pause'
-
-  btn2.id = 'stop'
-  btn2.title = 'stop'
-
-  btn3.id = 'rew'
-  btn3.title = '-5s'
-
-  btn4.id = 'ff'
-  btn4.title = '+5s'
-
-  aplayer.appendChild(btn1)
-  aplayer.appendChild(btn2)
-  aplayer.appendChild(btn3)
-  aplayer.appendChild(btn4)
-  let i1 = document.createElement('i')
-  let i2 = document.createElement('i')
-  let i3 = document.createElement('i')
-  let i4 = document.createElement('i')
-  btn1.appendChild(i1)
-  btn2.appendChild(i2)
-  btn3.appendChild(i3)
-  btn4.appendChild(i4)
-
-  i1.id = 'i1'
-  i1.className = 'iconfont icon-bofang1'
-  i1.innerHTML = 'play'
-
-  i2.id = 'i2'
-  i2.className = 'iconfont icon-tingzhi'
-  i2.innerHTML = 'stop'
-
-  i3.id = 'i3'
-  i3.className = 'iconfont icon-kuaituimiao'
-  i3.innerHTML = '-5s'
-
-  i4.id = 'i4'
-  i4.className = 'iconfont icon-kuaijinmiao'
-  i4.innerHTML = '+5s'
+function repeat(n, action) {
+  for (let i = 0; i < n; i++) {
+    action(i)
+  }
 }
+let labels = []
+repeat(3, (i) => {
+  labels.push(`Unit ${i + 1}`)
+})
+// console.log(labels)
 
-function toggle() {
-  //更改play/pause的显示,包括icon和text
-  ap.toggle()
-  changeToggle()
-  function changeToggle() {
-    if (ap.paused) {
-      i1.innerHTML = 'play'
-      i1.className = 'iconfont icon-bofang1'
-    } else {
-      i1.innerHTML = 'pause'
-      i1.className = 'iconfont icon-zanting'
-    }
+function noisy(f) {
+  //函数作为参数，显示调用和调用结果
+  //  ...args ：es6语法,接收多个参数
+  return (...args) => {
+    console.log('calling with', args)
+    let result = f(...args)
+    console.log('called with', args, ', returned', result)
+    return result
   }
 }
 
-function removeClass(element, classRomove) {
-  //删除一个元素的class
-  var classNames = ' ' + element.className + ' '
+// noisy(Math.min)(1, 2, 3)
 
-  classNames = classNames.replace(' ' + classRomove + ' ', ' ')
+function transparentWarpping(f)
 
-  //String.trim(classNames);
+function filter(array, test) {}
 
-  element.className = classNames
+var ANCESTRY_FILE = JSON.stringify([
+  {
+    name: 'Carolus Haverbeke',
+    sex: 'm',
+    born: 1832,
+    died: 1905,
+    father: 'Carel Haverbeke',
+    mother: 'Maria van Brussel',
+  },
+  {
+    name: 'Emma de Milliano',
+    sex: 'f',
+    born: 1876,
+    died: 1956,
+    father: 'Petrus de Milliano',
+    mother: 'Sophia van Damme',
+  },
+  {
+    name: 'Maria de Rycke',
+    sex: 'f',
+    born: 1683,
+    died: 1724,
+    father: 'Frederik de Rycke',
+    mother: 'Laurentia van Vlaenderen',
+  },
+  {
+    name: 'Jan van Brussel',
+    sex: 'm',
+    born: 1714,
+    died: 1748,
+    father: 'Jacobus van Brussel',
+    mother: 'Joanna van Rooten',
+  },
+  {
+    name: 'Philibert Haverbeke',
+    sex: 'm',
+    born: 1907,
+    died: 1997,
+    father: 'Emile Haverbeke',
+    mother: 'Emma de Milliano',
+  },
+  {
+    name: 'Jan Frans van Brussel',
+    sex: 'm',
+    born: 1761,
+    died: 1833,
+    father: 'Jacobus Bernardus van Brussel',
+    mother: null,
+  },
+  {
+    name: 'Pauwels van Haverbeke',
+    sex: 'm',
+    born: 1535,
+    died: 1582,
+    father: 'N. van Haverbeke',
+    mother: null,
+  },
+  {
+    name: 'Clara Aernoudts',
+    sex: 'f',
+    born: 1918,
+    died: 2012,
+    father: 'Henry Aernoudts',
+    mother: 'Sidonie Coene',
+  },
+  {
+    name: 'Emile Haverbeke',
+    sex: 'm',
+    born: 1877,
+    died: 1968,
+    father: 'Carolus Haverbeke',
+    mother: 'Maria Sturm',
+  },
+  {
+    name: 'Lieven de Causmaecker',
+    sex: 'm',
+    born: 1696,
+    died: 1724,
+    father: 'Carel de Causmaecker',
+    mother: 'Joanna Claes',
+  },
+  {
+    name: 'Pieter Haverbeke',
+    sex: 'm',
+    born: 1602,
+    died: 1642,
+    father: 'Lieven van Haverbeke',
+    mother: null,
+  },
+  {
+    name: 'Livina Haverbeke',
+    sex: 'f',
+    born: 1692,
+    died: 1743,
+    father: 'Daniel Haverbeke',
+    mother: 'Joanna de Pape',
+  },
+  {
+    name: 'Pieter Bernard Haverbeke',
+    sex: 'm',
+    born: 1695,
+    died: 1762,
+    father: 'Willem Haverbeke',
+    mother: 'Petronella Wauters',
+  },
+  {
+    name: 'Lieven van Haverbeke',
+    sex: 'm',
+    born: 1570,
+    died: 1636,
+    father: 'Pauwels van Haverbeke',
+    mother: 'Lievijne Jans',
+  },
+  {
+    name: 'Joanna de Causmaecker',
+    sex: 'f',
+    born: 1762,
+    died: 1807,
+    father: 'Bernardus de Causmaecker',
+    mother: null,
+  },
+  {
+    name: 'Willem Haverbeke',
+    sex: 'm',
+    born: 1668,
+    died: 1731,
+    father: 'Lieven Haverbeke',
+    mother: 'Elisabeth Hercke',
+  },
+  {
+    name: 'Pieter Antone Haverbeke',
+    sex: 'm',
+    born: 1753,
+    died: 1798,
+    father: 'Jan Francies Haverbeke',
+    mother: 'Petronella de Decker',
+  },
+  {
+    name: 'Maria van Brussel',
+    sex: 'f',
+    born: 1801,
+    died: 1834,
+    father: 'Jan Frans van Brussel',
+    mother: 'Joanna de Causmaecker',
+  },
+  {
+    name: 'Angela Haverbeke',
+    sex: 'f',
+    born: 1728,
+    died: 1734,
+    father: 'Pieter Bernard Haverbeke',
+    mother: 'Livina de Vrieze',
+  },
+  {
+    name: 'Elisabeth Haverbeke',
+    sex: 'f',
+    born: 1711,
+    died: 1754,
+    father: 'Jan Haverbeke',
+    mother: 'Maria de Rycke',
+  },
+  {
+    name: 'Lievijne Jans',
+    sex: 'f',
+    born: 1542,
+    died: 1582,
+    father: null,
+    mother: null,
+  },
+  {
+    name: 'Bernardus de Causmaecker',
+    sex: 'm',
+    born: 1721,
+    died: 1789,
+    father: 'Lieven de Causmaecker',
+    mother: 'Livina Haverbeke',
+  },
+  {
+    name: 'Jacoba Lammens',
+    sex: 'f',
+    born: 1699,
+    died: 1740,
+    father: 'Lieven Lammens',
+    mother: 'Livina de Vrieze',
+  },
+  {
+    name: 'Pieter de Decker',
+    sex: 'm',
+    born: 1705,
+    died: 1780,
+    father: 'Joos de Decker',
+    mother: 'Petronella van de Steene',
+  },
+  {
+    name: 'Joanna de Pape',
+    sex: 'f',
+    born: 1654,
+    died: 1723,
+    father: 'Vincent de Pape',
+    mother: 'Petronella Wauters',
+  },
+  {
+    name: 'Daniel Haverbeke',
+    sex: 'm',
+    born: 1652,
+    died: 1723,
+    father: 'Lieven Haverbeke',
+    mother: 'Elisabeth Hercke',
+  },
+  {
+    name: 'Lieven Haverbeke',
+    sex: 'm',
+    born: 1631,
+    died: 1676,
+    father: 'Pieter Haverbeke',
+    mother: 'Anna van Hecke',
+  },
+  {
+    name: 'Martina de Pape',
+    sex: 'f',
+    born: 1666,
+    died: 1727,
+    father: 'Vincent de Pape',
+    mother: 'Petronella Wauters',
+  },
+  {
+    name: 'Jan Francies Haverbeke',
+    sex: 'm',
+    born: 1725,
+    died: 1779,
+    father: 'Pieter Bernard Haverbeke',
+    mother: 'Livina de Vrieze',
+  },
+  {
+    name: 'Maria Haverbeke',
+    sex: 'm',
+    born: 1905,
+    died: 1997,
+    father: 'Emile Haverbeke',
+    mother: 'Emma de Milliano',
+  },
+  {
+    name: 'Petronella de Decker',
+    sex: 'f',
+    born: 1731,
+    died: 1781,
+    father: 'Pieter de Decker',
+    mother: 'Livina Haverbeke',
+  },
+  {
+    name: 'Livina Sierens',
+    sex: 'f',
+    born: 1761,
+    died: 1826,
+    father: 'Jan Sierens',
+    mother: 'Maria van Waes',
+  },
+  {
+    name: 'Laurentia Haverbeke',
+    sex: 'f',
+    born: 1710,
+    died: 1786,
+    father: 'Jan Haverbeke',
+    mother: 'Maria de Rycke',
+  },
+  {
+    name: 'Carel Haverbeke',
+    sex: 'm',
+    born: 1796,
+    died: 1837,
+    father: 'Pieter Antone Haverbeke',
+    mother: 'Livina Sierens',
+  },
+  {
+    name: 'Elisabeth Hercke',
+    sex: 'f',
+    born: 1632,
+    died: 1674,
+    father: 'Willem Hercke',
+    mother: 'Margriet de Brabander',
+  },
+  {
+    name: 'Jan Haverbeke',
+    sex: 'm',
+    born: 1671,
+    died: 1731,
+    father: 'Lieven Haverbeke',
+    mother: 'Elisabeth Hercke',
+  },
+  {
+    name: 'Anna van Hecke',
+    sex: 'f',
+    born: 1607,
+    died: 1670,
+    father: 'Paschasius van Hecke',
+    mother: 'Martijntken Beelaert',
+  },
+  {
+    name: 'Maria Sturm',
+    sex: 'f',
+    born: 1835,
+    died: 1917,
+    father: 'Charles Sturm',
+    mother: 'Seraphina Spelier',
+  },
+  {
+    name: 'Jacobus Bernardus van Brussel',
+    sex: 'm',
+    born: 1736,
+    died: 1809,
+    father: 'Jan van Brussel',
+    mother: 'Elisabeth Haverbeke',
+  },
+])
+var ancestry = JSON.parse(ANCESTRY_FILE)
+
+ancestry.map(p => p.name)
+for (let i = 0; i < array.length; i++){
+  
 }
-
-function stop() {
-  ap.seek(0)
-  ap.pause()
-}
-
-function rew() {
-  //快退5s
-  let targetNumber = ap.audio.currentTime - 5 > 0 ? ap.audio.currentTime - 5 : 0
-  ap.seek(targetNumber) //target time
-}
-
-function ff() {
-  //快进5s
-  let targetNumber =
-    ap.audio.currentTime + 5 < ap.audio.duration
-      ? ap.audio.currentTime + 5
-      : ap.audio.duration
-  ap.seek(targetNumber) //target time
-}
-//TODO: 左右建快进快退 & 再做一个音量条
